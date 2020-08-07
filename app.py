0d8d1e15c37d69e905fcdc3a0588acca51bfd3d4
Peter Chacko <workpraytoday@gmail.com>
2020-08-06 21:42:58 -0500

    changed secret key

--- app.py
+++ app.py
@@ -42,10 +42,10 @@ def after_request(response):
 
 
 # Configure session to use filesystem (instead of signed cookies)
-"""app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-"""
+
 Session(app)
 
 with app.app_context():
